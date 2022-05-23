16f926bf1e2a7f89d2f8a2f53166dba5254c94cb
richmanfx <richmanfx@yandex.ru>
2022-05-23 18:42:35 +0300

    Логирование запросов сервера

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,7 @@
 module Main where
   
---import Lib
 import LibHtml
-import System.Exit
-import Control.Monad
 
 main :: IO ()
 main = do
   webAppEntry
---main = renderToFile "index.html" mainHtml
